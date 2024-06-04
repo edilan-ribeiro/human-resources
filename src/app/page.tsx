fe1fcfdc07d11f6a839df632bb097fa61a18a194
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-06-03 21:53:19 -0300

    chore: add initial projet config

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import { Footer } from '@/components/Footer/Footer'
+import { Header } from '@/components/Header/Header'
+import { MobileMenu } from '@/components/Header/MobileMenu/MobileMenu'
+
+export default function Home() {
+  return (
+    <div className='w-vw flex justify-center overflow-x-hidden'>
+     <Header /> 
+      
+      {/*<main>
+        <div>
+          <p className='text-lg'>
+            Tail of the wind! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nostrum minima deleniti vitae
+            magni. Doloremque quos perspiciatis eligendi molestias quam, cumque ullam repellendus at itaque dignissimos
+            voluptas commodi ea molestiae?
+          </p>
+        </div>
+      </main> */}
+      {/* <Footer/> */}
+    </div>
+  )
+}
