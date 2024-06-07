9fdd87aebab52e6b412f6e11653a2b1238d2d587
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-06-07 16:04:16 -0300

    feat: add hero component to the project
    This section will provide the first information someone sees when entering the pages, it can be customized via props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import { Footer } from '@/components/Footer/Footer'
 import { Header } from '@/components/Header/Header'
 import { MobileMenu } from '@/components/Header/MobileMenu/MobileMenu'
+import { Hero } from '@/components/Hero/Hero'
 
 export default function Home() {
   return (
-    <div className='w-vw flex justify-center overflow-x-hidden'>
-     <Header /> 
-      
-      {/*<main>
-        <div>
-          <p className='text-lg'>
-            Tail of the wind! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nostrum minima deleniti vitae
-            magni. Doloremque quos perspiciatis eligendi molestias quam, cumque ullam repellendus at itaque dignissimos
-            voluptas commodi ea molestiae?
-          </p>
-        </div>
-      </main> */}
+    <div className='w-vw flex flex-col justify-center overflow-x-hidden'>
+      <Header />
+
+      <main>
+        <Hero
+          title='Seu Guia no Mercado de Trabalho'
+          subtitle='Serviços Personalizados para Profissionais e Empresas'
+          image='/img/home/people-working.png'
+          imageAlt='pessoas trabalhando'
+        />
+      </main>
       {/* <Footer/> */}
     </div>
   )
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+
+interface HeroProps {
+  title: string
+  subtitle?: string
+  image: string
+  imageAlt: string
+}
+
+export const Hero = ({ title, subtitle, image, imageAlt }: HeroProps) => {
+  return (
+    <section className='w-full'>
+      <div className='relative flex h-[570px] justify-center text-center'>
+        <div className='absolute inset-0 z-0 bg-black/30 shadow-[inset_0_0_49px_31px_rgba(0,0,0,0.50)]'></div>
+        <Image
+          className='z-[-1]'
+          src={image}
+          alt={imageAlt}
+          fill
+          style={{ objectFit: 'cover', objectPosition: 'top ' }}
+        />
+
+        <div className='max-w-desk relative z-[1] flex h-full flex-col items-center justify-center gap-6 text-gray-100'>
+          <h1 className='text-4xl font-semibold uppercase tracking-tight md:text-5xl'>{title}</h1>
+          <h2 className='text-xl tracking-wide md:text-2xl'>{subtitle}</h2>
+        </div>
+      </div>
+    </section>
+  )
+}
